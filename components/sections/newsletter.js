ebce7b8ed1627ccdd04c027cb0814a3986df1f5b
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-03 22:43:39 +0100

    Updating UI

--- components/sections/newsletter.js
+++ components/sections/newsletter.js
@@ -1,5 +1,7 @@
-import { Box, Container, Flex } from '@chakra-ui/react';
+import { Box, Container, Flex, Text } from '@chakra-ui/react';
 import NewsletterBg from '../../assets/images/newletter-background.png';
+import IntroBtn from '../intro/IntroBtn';
+import FormInput from '../ui/input/Input';
 import SectionTitle from '../ui/title/SectionTitle';
 
 const NewsLetter = function () {
@@ -22,6 +24,34 @@ const NewsLetter = function () {
             color='white'
             justifyContent='center'
           />
+          <Flex
+            bgColor='white'
+            p='1.5'
+            maxW='486px'
+            w='full'
+            borderRadius='42'
+            mt='6'
+            mx='auto'
+          >
+            <FormInput
+              variant='unstyled'
+              pl='4'
+              placeholder='Email address'
+              fontSize='16px'
+              color='brand.500'
+              flexGrow='1'
+            />
+            <IntroBtn hasBg={true} alignSelf='center'>
+              <Text
+                textAlign='center'
+                fontWeight='700'
+                fontSize='sm'
+                color='white'
+              >
+                Order now
+              </Text>
+            </IntroBtn>
+          </Flex>
         </Flex>
       </Container>
     </Box>
